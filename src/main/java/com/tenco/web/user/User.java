958ac7404e589fcc86be7b8bdb43523bb6d6260f
Yangsb <ysb5397@gmail.com>
2025-06-26 22:47:02 +0900

    user í클래스 설정

--- src/main/java/com/tenco/web/user/User.java
+++ src/main/java/com/tenco/web/user/User.java
@@ -1,5 +1,38 @@
 package com.tenco.web.user;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "user_tb")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Builder
+    public User(Long id, String username, String password, String email, Timestamp createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
 }

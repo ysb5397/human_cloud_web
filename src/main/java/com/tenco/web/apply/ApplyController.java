063fdaea9d176030f609742d0c9314a101ef962b
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-10 12:21:25 +0900

    지원수정중

--- src/main/java/com/tenco/web/apply/ApplyController.java
+++ src/main/java/com/tenco/web/apply/ApplyController.java
@@ -3,23 +3,42 @@ package com.tenco.web.apply;
 import com.tenco.web.user.User;
 import com.tenco.web.utis.Define;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
 public class ApplyController {
 
-    private ApplyService applyService;
+    private final ApplyService applyService;
 
+
+    // 이력서 제출 기능
     @PostMapping("/submit")
-    public String submitResume(@RequestParam(name = "id") int id,
-                               ApplyRequest.SaveDTO saveDTO, HttpSession session){
+    public String submitResume(ApplyRequest.SaveDTO saveDTO, HttpSession session){
 
         User sessionUser = (User) session.getAttribute(Define.DefineMessage.SESSION_USER);
         if(sessionUser == null) {
             return "redirect:/user/login-form";
         }
-        applyService.submitResume(id, saveDTO, sessionUser);
+        applyService.submitResume(saveDTO, sessionUser);
 
-        return "redirect:/user/apply";
+        return "redirect:/user";
     }
+
+    // 특정 유저가 제출한 지원 목록 보기
+    @GetMapping("/my-apply-list")
+    public String applyList(Model model, HttpSession session) {
+        User sessionUser = (User) session.getAttribute(Define.DefineMessage.SESSION_USER);
+        List<Apply> applyList = applyService.findByUserId(sessionUser.getId());
+        model.addAttribute("applyList", applyList);
+        return "user/my-apply-list";
+    }
+
+
 }
---
+++ src/main/java/com/tenco/web/resume/apply_resume/AppliedResume.java
@@ -0,0 +1,11 @@
+package com.tenco.web.resume.apply_resume;
+
+import com.tenco.web.company.Company;
+import com.tenco.web.resume.Resume;
+
+public class AppliedResume {
+
+    private Company company;
+    private Resume resume;
+
+}

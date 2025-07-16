844b0c223a0fb28bb08aa5e59e716f8bbc06a84b
sonjiyoon12 <1wldbs1908@gmail.com>
2025-07-16 14:42:28 +0900

    수정사항

--- src/main/java/com/tenco/web/companySub/CompanySub.java
+++ src/main/java/com/tenco/web/companySub/CompanySub.java
@@ -1,8 +1,11 @@
 package com.tenco.web.companySub;
 
+import com.tenco.web.company.Company;
+import com.tenco.web.user.User;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringExclude;
 
 @Data
 @NoArgsConstructor
@@ -15,6 +18,16 @@ public class CompanySub {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @ToStringExclude
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    @ToStringExclude
+    private Company company;
 
 
 
